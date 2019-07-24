a48a7796422aae0a27d0c1297e3f5a4c9c86a184
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-07-24 16:18:01 -0300

    Criando Lobby e Sala

--- Introdução Photon Pun 2 - Curso Udemy 2019/Assets/NetworkController.cs
+++ Introdução Photon Pun 2 - Curso Udemy 2019/Assets/NetworkController.cs
@@ -13,8 +13,7 @@ namespace IntroducaoPhotonUdemy {
 
         void Start() {
 
-            PhotonNetwork.ConnectUsingSettings(); //Conexão com photon
-            
+            ConectandoComServidorPhoton();
         }
 
         // Update is called once per frame
@@ -28,19 +27,44 @@ namespace IntroducaoPhotonUdemy {
             if(!isConnected) {
                 PhotonNetwork.ConnectUsingSettings(); //Conexão com photon
             }
-        }*/ 
+        }*/
 
-        public override void OnConnected() {//Chamado quando a conexao com photon é realizada
+        public void ConectandoComServidorPhoton() {
+            PhotonNetwork.ConnectUsingSettings(); //Conexão com photon
+        } 
+
+        //Chamado quando a conexao com photon é realizada
+        public override void OnConnected() {
             print("Conectado com o Photon");
            // isConnected = true;
         }
 
-        public override void OnConnectedToMaster() { //Chamado assim que a conexão é realizada e validada
+         //Chamado assim que a conexão é realizada e validada
+        public override void OnConnectedToMaster() {
             print("Conexão realizada e validada com sucesso");
             print("Server: " + PhotonNetwork.CloudRegion + " Ping: " + PhotonNetwork.GetPing()); //Local do servidor e ping
+            PhotonNetwork.JoinLobby(); //Entrar em um Lobby
         }
 
-        public override void OnDisconnected(DisconnectCause cause) { //Chamado ao desconectar do servidor da photon (ex: internet caiu)
+        //Ao entrar no Lobby
+        public override void OnJoinedLobby() {
+            print("Entrou no Lobby");
+            PhotonNetwork.JoinRandomRoom(); //Tentar entrar em uma sala randomicamente
+        }
+
+        //se nao existir sala ao tentar entrar randomicamente, vai retornar erro
+        //Será criada uma nova sala
+        public override void OnJoinRandomFailed(short returnCode, string message) {
+            PhotonNetwork.CreateRoom(null);
+        }
+
+        //Ao entrar em uma sala
+        public override void OnJoinedRoom() {
+            print("Jogador entrou em uma sala");
+        }
+
+        //Chamado ao desconectar do servidor da photon (ex: internet caiu)
+        public override void OnDisconnected(DisconnectCause cause) { 
             print("Desconetado do servidor: " + cause);
             PhotonNetwork.ConnectToRegion("cn"); //Usado para conectar em um servidor de determinada região //As regiões estão em: https://doc.photonengine.com/en-us/realtime/current/connection-and-authentication/regions
            // isConnected = false;
