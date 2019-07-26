268b2fb484ca1fbe4c988865554535b34f95e4b6
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-07-26 20:42:30 -0300

    Criando UI Lobby parte 1

---
+++ Unity 2018.3.14f1 - Nigthmare - Photon Pun 2 - Curso Udemy 2019/Assets/_MainGame/Scripts/Lobby.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nigthmare {
+    
+    public class Lobby : MonoBehaviour {
+
+        public GameObject panelLogin;
+        public GameObject panelLobby;
+
+        public Text txtLobbyAguardar;
+        public Text txtLobbyTimeStart;
+
+        public string lobbyTimeStartText = "Start Game in {0}...";
+
+        void Start() {
+
+            txtLobbyTimeStart.gameObject.SetActive(false);
+            PanelLoginActive();
+        }//Start
+
+        void Update() {
+
+            
+        }//Update
+
+        public void PanelLobbyActive() {
+
+            panelLobby.gameObject.SetActive(true);
+            panelLogin.gameObject.SetActive(false);
+        }
+
+        public void PanelLoginActive() {
+
+            panelLobby.gameObject.SetActive(false);
+            panelLogin.gameObject.SetActive(true);
+        }
+
+    }//Lobby
+    
+}//Nigthmare
+
+
---
+++ Unity 2018.3.14f1 - Nigthmare - Photon Pun 2 - Curso Udemy 2019/Assets/_MainGame/Scripts/NetworkController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Nigthmare {
+    
+    public class NetworkController : MonoBehaviourPunCallbacks {
+
+        public Lobby lobbyScript;
+
+        void Start() {
+
+            
+        }//Start
+
+        void Update() {
+
+            
+        }//Update
+
+        public override void OnConnected() {
+
+            print("OnConnected");
+        }
+
+        public override void OnConnectedToMaster() {
+
+            print("OnConnectedToMaster");
+            lobbyScript.PanelLobbyActive();
+        }
+
+        public override void OnDisconnected(DisconnectCause cause) {
+
+            print("OnDisconnected " + cause.ToString());
+            lobbyScript.PanelLoginActive();
+        }
+
+        public void BtnCancelar() {
+
+            PhotonNetwork.Disconnect(); //DisconnectByClientLogic
+        }
+
+        public void BtnLogin() {
+
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+    }//SCRIPTNAME
+    
+}//namespace
+
+
