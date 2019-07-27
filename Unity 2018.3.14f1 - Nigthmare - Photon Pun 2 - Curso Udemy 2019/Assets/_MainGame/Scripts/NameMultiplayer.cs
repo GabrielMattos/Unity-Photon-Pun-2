7223da192003c61daa2560051484d102d3f7ed52
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-07-27 02:51:04 -0300

    Configurando o Nome do Player

---
+++ Unity 2018.3.14f1 - Nigthmare - Photon Pun 2 - Curso Udemy 2019/Assets/_MainGame/Scripts/NameMultiplayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+namespace Nigthmare {
+    
+    public class NameMultiplayer : MonoBehaviour {
+
+        private PhotonView myPhotonView;
+
+        public Text txtPlayerName;
+
+        public GameObject playerCanvas;
+
+        void Start() {
+
+            myPhotonView = GetComponent<PhotonView>();
+
+            txtPlayerName.text = myPhotonView.Owner.NickName;
+            
+        }//Start
+
+        void Update() {
+
+            playerCanvas.transform.LookAt(Camera.main.transform);            
+        }//Update
+
+    }//SCRIPTNAME
+    
+}//namespace
+
+
