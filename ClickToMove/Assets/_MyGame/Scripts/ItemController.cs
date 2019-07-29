1fbf78e42ec871a4e90ee75455c87ccae4740282
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-07-29 19:27:35 -0300

    Pegando itens parte 1

---
+++ ClickToMove/Assets/_MyGame/Scripts/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClickToMove {
+    
+    public class ItemController : MonoBehaviour {
+
+        public string itemName;
+
+        void Start() {
+
+            
+        }//Start
+
+        void Update() {
+
+            
+        }//Update
+
+    }//SCRIPTNAME
+    
+}//namespace
+
+
---
+++ ClickToMove/Assets/_MyGame/Scripts/ItemSwitch.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace ClickToMove {
+    
+    public class ItemSwitch : MonoBehaviour {
+
+        public Transform weapon;
+
+        //Tempo para pegar o item
+        float timeForTake = 0.5f;
+        public float timeForTakeCurrent = 0f;
+
+        private PhotonView myPhotonView;
+
+        public RpcTarget myRpcTarget;
+
+        void Start() {
+
+            myPhotonView = GetComponent<PhotonView>();
+            
+        }//Start
+
+        void Update() {
+
+            timeForTakeCurrent += Time.deltaTime;
+        }//Update
+
+        private void OnTriggerStay(Collider target) {
+            
+            if(!myPhotonView.IsMine) {
+                return;
+            }
+
+            if(target.CompareTag("Item") && Input.GetKey(KeyCode.F) && timeForTakeCurrent >= timeForTake) {
+                timeForTakeCurrent = 0f;
+                myPhotonView.RPC("TakeItemRPC", myRpcTarget, target.GetComponent<ItemController>().itemName);
+            }
+        }
+
+        [PunRPC]
+        void TakeItemRPC(string nameItem) {
+
+            foreach (Transform itemWeapon in weapon)
+            {
+                if(itemWeapon.name == nameItem)
+                {
+                    if(!itemWeapon.gameObject.activeInHierarchy)
+                    {
+                        itemWeapon.gameObject.SetActive(true);
+                    }
+                } 
+                else 
+                {
+                    itemWeapon.gameObject.SetActive(false);
+                } 
+            }
+        }
+
+    }//SCRIPTNAME
+    
+}//namespace
+
+
