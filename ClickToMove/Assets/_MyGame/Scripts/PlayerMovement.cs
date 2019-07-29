a91d46f80284b7d2fab30d9d3bd2c0c7d553f2a9
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-07-29 17:09:26 -0300

    Criando o player

---
+++ ClickToMove/Assets/_MyGame/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ClickToMove {
+    
+    public class PlayerMovement : MonoBehaviour {
+
+        private NavMeshAgent myNavMeshAgent;
+
+        void Awake() {
+
+            myNavMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        void Start() {
+
+            
+        }//Start
+
+        void Update() {
+
+            ClickToMove();
+        }//Update
+
+        void ClickToMove() {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if(Input.GetMouseButtonDown(0)) {
+                if(Physics.Raycast(ray, out hit, 100)) {
+                    myNavMeshAgent.destination = hit.point;
+                }
+            }
+        }
+
+    }//SCRIPTNAME
+    
+}//namespace
+
+
